fix: transformer block bug fixed. Need to perform layer normalization on k and v as well, not just q

assignment1-basics/src/layers/attention.py
@@ -6,7 +6,7 @@ from jaxtyping import Float, Bool, Int
 from . import softmax, RoPE
 
 class MultiHeadAttention(nn.Module):
-    def __init__(self, d_model: int, num_heads: int, device: torch.device | None=None, pos_encoding: RoPE | None=None):
+    def __init__(self, d_model: int, num_heads: int, device: torch.device | None=None, pos_encoding: bool=False, theta: float=0, max_seq_len: int=1024): 
         """
         Initialize the multi-head attention layer.
         Args:
@@ -25,7 +25,10 @@ class MultiHeadAttention(nn.Module):
         self.w_o = nn.Parameter(nn.init.trunc_normal_(torch.rand(d_model, num_heads * self.d_v, device=device)))
         
         # initialize positional encoding
-        self.RoPE = pos_encoding 
+        self.RoPE = None
+        if pos_encoding:
+            self.RoPE = RoPE(theta, self.d_k, max_seq_len=max_seq_len, device=device)
+        
 
     def forward(self, 
         Q: Float[torch.Tensor, "... seq_len d_model"],

assignment1-basics/src/layers/feed_forward.py
@@ -15,11 +15,11 @@ class FeedForward(nn.Module):
         w3: Gate projection weight matrix [d_ff, d_model]
     """
     
-    def __init__(self, d_model: int, d_ff: int):
+    def __init__(self, d_model: int, d_ff: int, device: torch.device | None=None):
         super().__init__()
-        self.w1 = nn.Parameter(nn.init.trunc_normal_(torch.rand(d_ff, d_model)))
-        self.w2 = nn.Parameter(nn.init.trunc_normal_(torch.rand(d_model, d_ff)))
-        self.w3 = nn.Parameter(nn.init.trunc_normal_(torch.rand(d_ff, d_model)))
+        self.w1 = nn.Parameter(nn.init.trunc_normal_(torch.rand(d_ff, d_model, device=device)))
+        self.w2 = nn.Parameter(nn.init.trunc_normal_(torch.rand(d_model, d_ff, device=device)))
+        self.w3 = nn.Parameter(nn.init.trunc_normal_(torch.rand(d_ff, d_model, device=device)))
 
 
     def _load_weight(self, w1: torch.Tensor, w2: torch.Tensor, w3: torch.Tensor):

assignment1-basics/src/modules/transformer.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn as nn
 from jaxtyping import Float
 
-from src.layers import MultiHeadAttention, FeedForward, RMSNorm
+from src.layers import MultiHeadAttention, FeedForward, RMSNorm, RoPE
 
 class TransformerBlock(nn.Module):
     def __init__(self, d_model: int, num_heads: int, d_ff: int, max_seq_len: int=1024, pos_encoding: bool=False, theta: float=0, device: torch.device|None=None):
@@ -11,10 +11,10 @@ class TransformerBlock(nn.Module):
         self.h = num_heads
         self.d_ff = d_ff
         self.max_seq_len = max_seq_len
-        self.mask = torch.tril(torch.ones(max_seq_len, max_seq_len)).bool()
-        self.MHA = MultiHeadAttention(d_model, num_heads) 
-        self.FF = FeedForward(d_model, d_ff) 
-        self.LN = [RMSNorm(d_model) for i in range(2)]
+        self.mask = torch.tril(torch.ones(max_seq_len, max_seq_len, device=device)).bool()
+        self.MHA = MultiHeadAttention(d_model, num_heads, device=device, theta=theta, max_seq_len=max_seq_len, pos_encoding=pos_encoding) 
+        self.FF = FeedForward(d_model, d_ff, device=device) 
+        self.LN = [RMSNorm(d_model, device=device) for _ in range(2)]
         
 
     def forward(self, q: Float[torch.Tensor, " batch seq_len d_model"],
@@ -25,7 +25,7 @@ class TransformerBlock(nn.Module):
         mask = self.mask[:seq_len, :seq_len]
 
         # compute MHA y = x + MultiHeadSelfAttention(RMSNorm(x))
-        q = q + self.MHA.forward(self.LN[0].forward(q), k, v, mask=mask)
+        q = q + self.MHA.forward(self.LN[0].forward(q), self.LN[0].forward(k), self.LN[0].forward(v), mask=mask)
 
         # compute fast forward layer
         q = q + self.FF.forward(self.LN[1].forward(q))

assignment1-basics/tests/adapters.py
@@ -205,9 +205,7 @@ def run_multihead_self_attention_with_rope(
     # Generate token_positions if not provided
     # perform positional encoding to Q and K
     # RoPE embedding dimension must be the head embedding dimension (d_model // num_heads)
-    d_k = d_model // num_heads
-    pos_encoding = RoPE(theta, d_k, max_seq_len=max_seq_len)
-    multi_head_attention = MultiHeadAttention(d_model, num_heads, pos_encoding=pos_encoding)
+    multi_head_attention = MultiHeadAttention(d_model, num_heads, pos_encoding=True, theta=theta, max_seq_len=max_seq_len)
     multi_head_attention.w_q= nn.Parameter(q_proj_weight)
     multi_head_attention.w_k= nn.Parameter(k_proj_weight)
     multi_head_attention.w_v= nn.Parameter(v_proj_weight)
@@ -313,7 +311,7 @@ def run_transformer_block(
         Float[Tensor, "batch sequence_length d_model"] Tensor with the output of
         running the Transformer block on the input features while using RoPE.
     """
-    block = TransformerBlock(d_model, num_heads, d_ff, max_seq_len)
+    block = TransformerBlock(d_model, num_heads, d_ff, max_seq_len, pos_encoding=True, theta=theta)
     # set the weights
     block.MHA.w_q = nn.Parameter(weights['attn.q_proj.weight'])
     block.MHA.w_k = nn.Parameter(weights['attn.k_proj.weight'])